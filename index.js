21c57af6b3e7449f1e3f89752c71a8f9fb855a64
Tidustmp <tidus.tmp@icloud.com>
2021-01-05 15:16:31 +0800

    2nd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 var path = require("path");
-var cors = require("cors");
+// var cors = require("cors");
 const app = express();
 const port = 3000;
 //runkit.com/
 
-app.use(cors());
+// app.use(cors());
 app.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
